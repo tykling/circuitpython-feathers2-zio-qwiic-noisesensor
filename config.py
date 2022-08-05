b46b21458f04ecd473d19079b04238d70c24eb93
Thomas Steen Rasmussen <thomas@gibfest.dk>
2022-08-05 18:02:59 +0200

    run without tls and add missing config setting

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class Config:
     wifi_ssid = "bornhack-nat"
     mqtt_broker_hostname = "mqtt.camp.bornhack.dk"
-    mqtt_broker_port = 8883
+    mqtt_broker_port = 1883
     mqtt_topic = "/some/mqtt/topic/sensorname"
+    send_interval_seconds = 5
